26bf5ce7e0c00c6007db23b26d8923ea8d1d7456
yuwenhao <yuwenhao6882690@sina.com>
2019-11-06 20:18:50 +0800

    第一次提交

---
+++ src/com/risen/Test.java
@@ -0,0 +1,12 @@
+package com.risen;
+
+/*** @Auther: http://www.bjsxt.com 
+ * @Date: 2019/11/6 
+ * @Description: com.risen
+ * @version: 1.0
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
